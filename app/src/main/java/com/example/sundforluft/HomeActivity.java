e6cf5e5d21d23dda6c9ddc1841e4021e1008a465
Nicklas185105 <43750236+Nicklas185105@users.noreply.github.com>
2019-10-30 10:55:08 +0100

    prototype

---
+++ app/src/main/java/com/example/sundforluft/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.sundforluft;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        //overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}

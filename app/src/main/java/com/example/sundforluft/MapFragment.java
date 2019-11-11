a863646794911ae530478035d5118e6a11c9b8e3
Frimann <frimse.wow@gmail.com>
2019-11-11 11:35:39 +0100

    Added Google Maps activity as fragment.

--- app/src/main/java/com/example/sundforluft/MapFragment.java
+++ app/src/main/java/com/example/sundforluft/MapFragment.java
@@ -1,6 +1,10 @@
 package com.example.sundforluft;
 
+import android.Manifest;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +13,73 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
 public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap mMap;
+    private Geocoder mGeo;
+    private HashMap<String, Address> addresses;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_map, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+
+        } catch (   Exception e){
+            e.printStackTrace();
+        }
+
+        mGeo = new Geocoder(this.getContext());
+        addresses = new HashMap<>();
+        try {
+            addresses.put("DTU Lyngby", mGeo.getFromLocationName("DTU Lyngby",1).get(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        mMapView.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+            addresses.forEach((k,v)-> {
+                LatLng latLng = new LatLng(v.getLatitude(),v.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(latLng).title(k).snippet("Description"));
+            });
+
+
+
+
+            /* Should request user's location as a LatLng, DTU Lyngby temp value */
+            LatLng currentLoc = new LatLng(addresses.get("DTU Lyngby").getLatitude(), addresses.get("DTU Lyngby").getLongitude());
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLoc).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        });
+        return rootView;
+
     }
 }

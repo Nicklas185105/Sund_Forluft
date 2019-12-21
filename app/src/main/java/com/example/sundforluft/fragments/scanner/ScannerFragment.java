60501754c257c1bbacfc4c1224a0f67b269e026c
hansi <hanskroght@gmail.com>
2019-12-21 10:22:33 +0100

    QR scanner, Vi skal vide hvad QR koden giver tilbage

--- app/src/main/java/com/example/sundforluft/fragments/scanner/ScannerFragment.java
+++ app/src/main/java/com/example/sundforluft/fragments/scanner/ScannerFragment.java
@@ -1,21 +1,98 @@
 package com.example.sundforluft.fragments.scanner;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.sundforluft.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class ScannerFragment extends Fragment {
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    BarcodeDetector barcodeDectector;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scanner, container, false);
+        View view = inflater.inflate(R.layout.fragment_rankliste_rankin, container, false);
+        surfaceView = view.findViewById(R.id.qrCamera);
+        textView = view.findViewById(R.id.qrText);
+
+
+        barcodeDectector = new BarcodeDetector.Builder(getActivity()).setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(getActivity(), barcodeDectector).setRequestedPreviewSize(640,480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try
+                {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDectector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() != 0)
+                {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator) getActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+
+                        }
+                    });
+                }
+            }
+        });
+
+      return view;
     }
 }

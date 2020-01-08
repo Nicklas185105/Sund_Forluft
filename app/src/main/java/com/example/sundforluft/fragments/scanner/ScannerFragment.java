3b02f756d0529fb2071a2158fe74be2975a0bf4a
Hans Krogh Thomsen <Hanskroght@gmail.com>
2020-01-08 18:07:54 +0100

    Vi har glemt telefonen

--- app/src/main/java/com/example/sundforluft/fragments/scanner/ScannerFragment.java
+++ app/src/main/java/com/example/sundforluft/fragments/scanner/ScannerFragment.java
@@ -13,6 +13,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,75 +25,68 @@ import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.IOException;
 
-public class ScannerFragment extends Fragment {
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView textView;
-    BarcodeDetector barcodeDectector;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerFragment extends Fragment implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_rankliste_rankin, container, false);
-        surfaceView = view.findViewById(R.id.qrCamera);
-        textView = view.findViewById(R.id.qrText);
+        View view = inflater.inflate(R.layout.fragment_scanner, container, false);
 
 
-        barcodeDectector = new BarcodeDetector.Builder(getActivity()).setBarcodeFormats(Barcode.QR_CODE).build();
+        scannerView = view.findViewById(R.id.zxscan);
+        txtResult = view.findViewById(R.id.txt_result);
 
-        cameraSource = new CameraSource.Builder(getActivity(), barcodeDectector).setRequestedPreviewSize(640,480).build();
+        ScannerFragment self = this;
 
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try
-                {
-                    cameraSource.start(holder);
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-            }
+        //Request permission
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        scannerView.setResultHandler(self);
+                        scannerView.startCamera();
+                    }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(getActivity(), "You need to accept permission", Toast.LENGTH_LONG).show();
+                    }
 
-            }
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
 
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-        });
-
-        barcodeDectector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                if(qrCodes.size() != 0)
-                {
-                    textView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator) getActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-                            textView.setText(qrCodes.valueAt(0).displayValue);
-
-                        }
-                    });
-                }
-            }
-        });
-
-      return view;
+                    }
+                }).check();
+        return view;
     }
+
+    @Override
+    public void onDestroy() {
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+    @Override
+    public void handleResult(Result rawResult){
+        // QR scan result.
+        String deviceId = rawResult.getText();
+        
+
+
+    }
+
 }

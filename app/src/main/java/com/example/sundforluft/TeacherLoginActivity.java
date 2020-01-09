c8f487e04256bb9d3cec235b801c3d8bcd05835c
Nicklas185105 <s185105@student.dtu.dk>
2020-01-09 15:22:38 +0100

    ændret navn til teacher istedet for laerer

--- app/src/main/java/com/example/sundforluft/TeacherLoginActivity.java
+++ app/src/main/java/com/example/sundforluft/TeacherLoginActivity.java
@@ -2,7 +2,6 @@ package com.example.sundforluft;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +27,7 @@ public class TeacherLoginActivity extends AppCompatActivity implements View.OnCl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_laerer_login);
+        setContentView(R.layout.activity_teacher_login);
 
         button = findViewById(R.id.button);
         button.setOnClickListener(this);

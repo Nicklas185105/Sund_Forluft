4b19a88a8018c69aff8026cf70119888a19bc918
Nicklas185105 <43750236+Nicklas185105@users.noreply.github.com>
2020-01-13 14:03:46 +0100

    Henter skole navne, mangler at få dem displaied i en dropdown

--- app/src/main/java/com/example/sundforluft/admin/RemoveSchoolActivity.java
+++ app/src/main/java/com/example/sundforluft/admin/RemoveSchoolActivity.java
@@ -2,15 +2,33 @@ package com.example.sundforluft.admin;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.sundforluft.DAL.SchoolsLocator;
 import com.example.sundforluft.R;
+import com.example.sundforluft.services.Globals;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class RemoveSchoolActivity extends AppCompatActivity {
+public class RemoveSchoolActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     Toolbar toolbar;
+    Spinner dropdown;
+    String[] items;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    long schoolCount;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +43,84 @@ public class RemoveSchoolActivity extends AppCompatActivity {
         // Arrow Click
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        dropdown = findViewById(R.id.dropdown);
+        myRef = database.getReference("schools");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        /*for (DataSnapshot snap: dataSnapshot.getChildren()){
+                            Log.e(snap.getKey(), snap.getChildrenCount() + "");
+                        }*/
+                schoolCount = dataSnapshot.getChildrenCount();
+                System.out.println(schoolCount);
+                items = new String[(int)schoolCount+1];
+                for (i = 0; i < schoolCount; i++){
+                    myRef = database.getReference("schools/" + i);
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            items[i] = dataSnapshot.getValue().toString();
+                            //System.out.println(dataSnapshot.getValue());
+                            System.out.println(items[i]);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        // Display de skoler der bliver hentet i dropdown menuen
+
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);*/
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    void getSchoolCount(){
+        myRef = database.getReference("schools");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        /*for (DataSnapshot snap: dataSnapshot.getChildren()){
+                            Log.e(snap.getKey(), snap.getChildrenCount() + "");
+                        }*/
+                RemoveSchoolActivity.this.schoolCount = dataSnapshot.getChildrenCount();
+                //System.out.println(RemoveSchoolActivity.this.schoolCount);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override

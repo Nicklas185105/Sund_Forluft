53b22b6508900a1b1ab6fc046161819d13fda2e2
Nicklas185105 <s185105@student.dtu.dk>
2020-01-20 15:24:43 +0100

    slettet ikke brugte filer (Old Data broker), gået over til cloud i stedet

--- app/src/main/java/com/example/sundforluft/services/DataBroker/AirQualityDataModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.sundforluft.services.DataBroker;
-
-import java.time.LocalDateTime;
-import java.util.Date;
-
-public class AirQualityDataModel {
-    public LocalDateTime Date;
-    public double CO2;
-}
--- app/src/main/java/com/example/sundforluft/services/DataBroker/DataBroker.java
+++
@@ -1,9 +0,0 @@
-package com.example.sundforluft.services.DataBroker;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface DataBroker {
-    boolean load(LocalDateTime start, LocalDateTime end);
-    List<AirQualityDataModel> getData();
-}
